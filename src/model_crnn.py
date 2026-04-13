805e16d70bc1dc0f7051c75f6831182c14f00a0a
sachi7125 <sachijain@Sachis-MacBook-Air.local>
2026-04-14 04:53:51 +0530

    feat: add CRNN loader placeholder

---
+++ src/model_crnn.py
@@ -0,0 +1,7 @@
+import torch
+
+def load_crnn(weights_path: str, device: str = "cpu"):
+    # You will paste your CRNN class here later
+    # For now this is a placeholder
+    model = None
+    return model
